de4da48f66a013a5e7c336fe0294a889e8237149
David <david.levin2@live.biu.ac.il>
2024-03-07 16:22:08 +0200

    feat(models): define dataclasses

---
+++ gbfs/models/data_view.py
@@ -0,0 +1,35 @@
+from typing import Dict
+
+import numpy as np
+import pandas as pd
+from dataclasses import dataclass
+
+
+@dataclass
+class DataCollection:
+    x: pd.DataFrame
+    y: pd.DataFrame
+    x_y: pd.DataFrame
+
+
+@dataclass
+class DataProps:
+    n_labels: int
+    labels: np.ndarray
+
+    n_features: int
+    features: pd.DataFrame
+    feature_costs: Dict[str, float]
+
+
+@dataclass
+class DataView:
+    data_props: DataProps
+    data: DataCollection
+    norm_data: DataCollection
+
+
+@dataclass
+class FeaturesGraph:
+    sep_matrix: np.ndarray
+    reduced_sep_matrix: np.ndarray
