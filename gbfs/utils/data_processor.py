cb21ea3f71ee81adb3c56560432d11105419441c
David <david.levin2@live.biu.ac.il>
2024-03-07 16:21:46 +0200

    feat(utils): utils functions

---
+++ gbfs/utils/data_processor.py
@@ -0,0 +1,107 @@
+import pandas as pd
+from typing import Optional, Dict
+from sklearn.preprocessing import MinMaxScaler
+
+from gbfs.models.data_view import DataView, DataCollection, DataProps
+
+EPSILON = 1e-10
+
+
+class DataProcessor:
+    """
+    The DataProcessor class is responsible for reading a dataset from a path,
+    normalizing the data, and creating DataView objects with the dataset
+    properties.
+    """
+
+    def __init__(self, dataset_path: str, label_column: str = 'class', cost_column: Optional[str] = None):
+        """
+        Initializes the DataProcessor with the dataset path and optional column names for label and cost.
+
+        :param dataset_path: The file path to the dataset (CSV file expected).
+        :param label_column: The name of the column to use as the label.
+        :param cost_column: The name of the column to use as the feature cost (optional).
+        """
+        self.dataset_path = dataset_path
+        self.label_column = label_column
+        self.cost_column = cost_column
+
+    def run(self) -> DataView:
+        """
+        Executes the data processing pipeline: reading the data, normalizing it,
+        and creating a DataView object.
+
+        :return: A DataView object with the original and normalized data and data properties.
+        """
+        data = pd.read_csv(self.dataset_path)
+        norm_data = self._normalize_data(data)
+
+        data_collections = self._create_data_collections(data, norm_data)
+        data_props = self._compute_data_properties(data_collections['original'])
+
+        return DataView(data=data_collections['original'],
+                        norm_data=data_collections['normalized'],
+                        data_props=data_props)
+
+    def _normalize_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Normalizes the data using Min-Max scaling.
+
+        :param data: The data to normalize.
+        :return: The normalized data as a pandas DataFrame.
+        """
+        scaler = MinMaxScaler()
+        cols_to_normalize = data.columns.difference([self.label_column])
+        data[cols_to_normalize] = scaler.fit_transform(data[cols_to_normalize] + EPSILON)
+        return data
+
+    def _create_data_collections(self, original_data: pd.DataFrame, normalized_data: pd.DataFrame) -> Dict[
+        str, DataCollection]:
+        """
+        Creates collections for the original and normalized data.
+
+        :param original_data: The original data as a pandas DataFrame.
+        :param normalized_data: The normalized data as a pandas DataFrame.
+        :return: A dictionary with keys 'original' and 'normalized' mapping to their respective DataCollection instances.
+        """
+        return {
+            'original': DataCollection(x=original_data.drop(self.label_column, axis=1),
+                                       y=pd.DataFrame(original_data[self.label_column]),
+                                       x_y=original_data),
+            'normalized': DataCollection(x=normalized_data.drop(self.label_column, axis=1),
+                                         y=pd.DataFrame(normalized_data[self.label_column]),
+                                         x_y=normalized_data)
+        }
+
+    def _compute_data_properties(self, data: DataCollection) -> DataProps:
+        """
+        Computes the data properties, such as label count and feature count.
+
+        :param data: A DataCollection instance of the original data.
+        :return: A DataProps instance with the computed properties.
+        """
+        labels = data.y[self.label_column].unique()
+        features = data.x.columns
+        feature_costs = self._compute_feature_costs(data.x)
+
+        return DataProps(labels=labels,
+                         n_labels=len(labels),
+                         features=features,
+                         n_features=len(features),
+                         feature_costs=feature_costs)
+
+    def _compute_feature_costs(self, features: pd.DataFrame) -> Dict[str, float]:
+        """
+        Computes the costs associated with each feature.
+
+        :param features: A pandas DataFrame containing the feature data.
+        :return: A dictionary mapping each feature to its cost.
+        """
+        if self.cost_column:
+            try:
+                costs = features[self.cost_column].fillna(EPSILON).tolist()
+                return {feature: cost for feature, cost in zip(features.columns, costs)}
+            except KeyError:
+                raise KeyError(f"Cost column '{self.cost_column}' does not exist in the data.")
+        else:
+            return {feature: 1.0 for feature in features.columns}
---
+++ gbfs/utils/distance.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pandas as pd
+from typing import Tuple
+
+
+def get_classes(data: pd.DataFrame, feature: str, label_1: str, label_2: str, label_column: str) -> Tuple[
+    np.ndarray, np.ndarray]:
+    """
+    Retrieves classes based on specified feature and labels.
+
+    :param data: DataFrame containing the dataset.
+    :param feature: The feature for which the classes are to be retrieved.
+    :param label_1: The first label for class comparison.
+    :param label_2: The second label for class comparison.
+    :param label_column: The name of the label column in the dataset.
+    :return: Tuple of numpy ndarrays for the two classes.
+    """
+    c_1 = data.loc[data[label_column] == label_1, feature].to_numpy()
+    c_2 = data.loc[data[label_column] == label_2, feature].to_numpy()
+    return c_1, c_2
+
+
+def get_distance(metric: str, c_1: np.ndarray, c_2: np.ndarray) -> float:
+    """
+    Computes the distance between two classes based on the specified metric.
+
+    :param metric: The name of the metric to use for computing distance.
+    :param c_1: Class 1 data points.
+    :param c_2: Class 2 data points.
+    :return: The computed distance metric.
+    """
+    if metric == 'jm':
+        return jm_distance(c_1, c_2)
+    else:
+        raise ValueError(f"Unsupported metric '{metric}' provided.")
+
+
+def jm_distance(p: np.ndarray, q: np.ndarray):
+    b = bhattacharyya_distance(p, q)
+    jm = 2 * (1 - np.exp(-b))
+    return jm
+
+
+def bhattacharyya_distance(p: np.ndarray, q: np.ndarray):
+    mean_p, mean_q = p.mean(), q.mean()
+    std_p = p.std() if p.std() != 0 else 0.00000000001
+    std_q = q.std() if q.std() != 0 else 0.00000000001
+
+    var_p, var_q = std_p ** 2, std_q ** 2
+    b = (1 / 8) * ((mean_p - mean_q) ** 2) * (2 / (var_p + var_q)) + \
+        0.5 * np.log((var_p + var_q) / (2 * (std_p * std_q)))
+    return b
