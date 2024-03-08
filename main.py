7444f2f121d5d2799a7dff12913bc5c35ba64b20
David <david.levin2@live.biu.ac.il>
2024-03-08 17:50:12 +0200

    chore: refactor documentation

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from sklearn.manifold import TSNE
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from gbfs.algorithms.gbafs import GBAFS
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    tsne = TSNE(n_components=2, perplexity=10)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    x = GBAFS(
+        dataset_path='cardiotocography.csv',
+        separability_metric='jm',
+        dim_reducer_model=tsne,
+    )
+    y = x.select_features()
+    x.plot_feature_space()
